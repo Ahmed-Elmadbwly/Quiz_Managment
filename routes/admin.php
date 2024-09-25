463b83331a62188b15292375bbffe1b46cd9ae49
seadawy <k9335551@gmail.com>
2024-09-25 06:46:14 +0300

    adminTemp and Layout

---
+++ routes/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/admin.php';
